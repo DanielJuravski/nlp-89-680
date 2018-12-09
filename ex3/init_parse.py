1585d10871efef7fd0d0dd5acecd5077e66ac87f
eyalorbach <eyal@mac.com>
2018-12-09 17:57:13 +0200

    refactor sentence parse and change prep to addition in dep

--- ex3/init_parse.py
+++ ex3/init_parse.py
@@ -1,6 +1,8 @@
 import sys
 from collections import Counter
 
+import datetime
+
 CONTEXT_TEGSET = {'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ',  # Verb
                 'NN', 'NNP', 'NNPS', 'NNS',  # Noun
                 'JJ', 'JJR', 'JJS',  # Adjective
@@ -26,35 +28,29 @@ def all_sentence_parse(input_data, output_file):
     with open(input_data, 'r') as f_in:
         with open(output_file, 'w') as f_out:
             file_lines = f_in.readlines()
-            start_line = 0
-            line_words = set()
+            sentence_lines = []
             for file_line_i in range(len(file_lines)):
                 file_line = file_lines[file_line_i]
-                line = file_line.split()
-                if line:  # if not blank line
-                    word = line[2]  # lemma
-                    word_tag = line[4] # lemma's tag
-                    if word_tag in CONTEXT_TEGSET:
-                        word_counts[word] += 1
-                        # if word appeared twice (or more) in sentence, don't add it twice (or more) and move to the next word
-                        if word not in line_words:
-                            line_words.add(word)
-                            for in_line_i in range(start_line, start_line+9999):
-                                in_line = file_lines[in_line_i]
-                                in_line = in_line.split()
-                                if in_line:  # if not blank line
-                                    context = in_line[2]  # lemma
-                                    word_tag = in_line[4]  # lemma's tag
-                                    if word_tag in CONTEXT_TEGSET:
-                                        if word != context:  # if context word is not the word we work on it
-                                            string2file = word + ' ' + context + '\n'
-                                            f_out.write(string2file)
-                                            #print string2file
-                                else:
-                                    break
-                else:  # move to the next word
-                    start_line = file_line_i+1
-                    line_words = set()
+                stripped = file_line.strip()
+                if stripped:  # if not blank line
+                   sentence_lines.append(file_line)
+                else:  # process
+                    for word_i in range(len(sentence_lines)):
+                        line = sentence_lines[word_i].split()
+                        word = line[2]  # lemma
+                        word_tag = line[4] # lemma's tag
+                        if word_tag in CONTEXT_TEGSET:
+                            word_counts[word] += 1
+                            for context_i in range(len(sentence_lines)):
+                                if(word_i != context_i):
+                                    context_line = sentence_lines[context_i].split()
+                                    context = context_line[2]  # lemma
+                                    context_tag = context_line[4]  # lemma's tag
+                                    if context_tag in CONTEXT_TEGSET:
+                                        string2file = word + ' ' + context + '\n'
+                                        f_out.write(string2file)
+                    sentence_lines = []
+
 
 
 def getWindow(start_line, file_lines, word):
@@ -152,13 +148,12 @@ def dependency_parse(input_data, output_file):
                         if word_tag in CONTEXT_TEGSET:
                             if head != 0:  # ROOT
                                 word_counts[word] += 1
-                                if head_tag != 'IN':
-                                    att1 = head_word + "|" + deprel + "|<"  # < is relation direction
-                                    att2 = word + "|" + deprel + "|>"  # < is relation direction
-                                    # we add all contexts to word
-                                    this_line_word_context.append((word, att1))
-                                    this_line_word_context.append((head_word, att2))
-                                else:
+                                att1 = head_word + "|" + deprel + "|<"  # < is relation direction
+                                att2 = word + "|" + deprel + "|>"  # < is relation direction
+                                # we add all contexts to word
+                                this_line_word_context.append((word, att1))
+                                this_line_word_context.append((head_word, att2))
+                                if head_tag == 'IN':
                                     head_head_ID = int(sentence_lines[head - 1][6])
                                     head_head_word = sentence_lines[head_head_ID-1][2]
                                     att1 = head_head_word + "|" + deprel + "|" + head_word + "|<"
@@ -176,6 +171,7 @@ def dependency_parse(input_data, output_file):
 
 
 if __name__ == '__main__':
+    print(datetime.datetime.now())
     parse_type = sys.argv[1]
     input_data = sys.argv[2]
     output_file = sys.argv[3]
@@ -194,6 +190,8 @@ if __name__ == '__main__':
             count = word_counts[w]
             if count > word_min_num:
                 f.write("%s %d\n" % (w, count))
+
+    print(datetime.datetime.now())
 
 
 
