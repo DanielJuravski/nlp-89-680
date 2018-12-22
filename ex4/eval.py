368ae2e03ea2bbaaab5420cd0d0ab0d31e1c6c38
user <juravski95@gmail.com>
2018-12-22 16:44:54 +0200

    sorted files eval

--- ex4/eval.py
+++ ex4/eval.py
@@ -36,60 +36,100 @@ def get_annotation_data(annotation_file, gold):
 
 
 def compare_values(sen_id, gold_values, pred_values):
-    with open('statistics/TP.txt', 'a') as f_TP:
-        with open('statistics/FP.txt', 'a') as f_FP:
-            gold = (gold_values)
-            pred = (pred_values)
-            TP = FP = FN = 0
-            match = False
-            for pred_val in pred:
-                for gold_val in gold:
-                    if gold_val[utils.NER1] == pred_val[utils.NER1] and \
-                            gold_val[utils.RE] == pred_val[utils.RE] and \
-                            gold_val[utils.NER2] == pred_val[utils.NER2]:
-                        TP += 1
-                        string = sen_id + " " + ' '.join(pred_val)
-                        f_TP.write(string)
-                        match = True
-                        gold.remove(gold_val)
-                        break
-                if not match:
-                    FP += 1
-                    string = sen_id + " " + ' '.join(pred_val)
-                    f_FP.write(string)
-                match = False
+    TP_sent = []
+    FP_sent = []
+    TP = FP = 0
+    match = False
 
-            # sanity
-            if TP+FP > len(pred_values):
-                print "ERROR in compare_values()"
+    for pred_val in pred_values:
+        for gold_val in gold_values:
+            if gold_val[utils.NER1] == pred_val[utils.NER1] and \
+                    gold_val[utils.RE] == pred_val[utils.RE] and \
+                    gold_val[utils.NER2] == pred_val[utils.NER2]:
+                TP += 1
+                string = sen_id + "\t" + '\t'.join(pred_val)
+                TP_sent.append(string)
+                match = True
+                gold_values.remove(gold_val)
+                break
+        if not match:
+            FP += 1
+            string = sen_id + "\t" + '\t'.join(pred_val)
+            FP_sent.append(string)
+        match = False
 
-    return TP, FP, FN
+    # sanity
+    if TP+FP > len(pred_values):
+        print "ERROR in compare_values()"
+
+    return TP, FP, TP_sent, FP_sent
 
 
 def get_stat(gold_annotation_data, pred_annotation_data):
     TP = FP = FN = 0.0
-    open('statistics/TP.txt', 'w').close()
-    open('statistics/FP.txt', 'w').close()
-    open('statistics/FN.txt', 'w').close()
+    TP_sents = []
+    FP_sents = []
+    FN_sents = []
 
     for pred_sen_id, pred_value in pred_annotation_data.iteritems():
         if gold_annotation_data.has_key(pred_sen_id):
             gold_value = gold_annotation_data[pred_sen_id]
-            this_TP, this_FP, this_FN = compare_values(pred_sen_id, gold_value, pred_value)
+            this_TP, this_FP, this_TP_sent, this_FP_sent = compare_values(pred_sen_id, gold_value, pred_value)
             TP += this_TP
             FP += this_FP
-            FN += this_FN
+            TP_sents += this_TP_sent
+            FP_sents += this_FP_sent
         else:
             FP += len(pred_value)
+            for val in pred_value:
+                string = pred_sen_id + "\t" + '\t'.join(val)
+                FP_sents.append(string)
 
-    with open('statistics/FN.txt', 'a') as f_FN:
-        for gold_sen_id, gold_value in gold_annotation_data.iteritems():
-            for val in gold_value:
-                string = gold_sen_id + " " + ' '.join(val)
-                f_FN.write(string)
-            FN += len(gold_value)
+    for gold_sen_id, gold_value in gold_annotation_data.iteritems():
+        for val in gold_value:
+            string = gold_sen_id + "\t" + '\t'.join(val)
+            FN_sents.append(string)
+        FN += len(gold_value)
 
-    return TP, FP, FN
+    return TP, FP, FN, TP_sents, FP_sents, FN_sents
+
+
+def print_stats(TP, FP, FN):
+    print "TP: " + str(TP)
+    print "FP: " + str(FP)
+    print "FN: " + str(FN)
+
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1 = 2 * (recall * precision) / (recall + precision)
+
+    print
+    print ("Precision = %.4f%%" % (precision * 100))
+    print ("Recall = %.4f%%" % (recall * 100))
+    print ("F1 = %.4f%%" % (f1 * 100))
+
+
+def write2file(sentences, file_name):
+    sent_ids = []
+    for sent in sentences:
+        sent_id = sent.split()[0]
+        sent_ids.append(sent_id)
+    sorted_ids = utils.get_sorted_ids(sent_ids)
+
+    with open(file_name, 'w') as f:
+        for sorted_sent_id in sorted_ids:
+            for sent in sentences:
+                sent_id = sent.split()[0]
+                if sent_id == sorted_sent_id:
+                    f.write(sent)
+                    break
+
+
+def write_stat2file(TP_sents, FP_sents, FN_sents):
+    write2file(TP_sents, 'statistics/TP.txt')
+    write2file(FP_sents, 'statistics/FP.txt')
+    write2file(FN_sents, 'statistics/FN.txt')
+
 
 
 if __name__ == '__main__':
@@ -104,17 +144,9 @@ if __name__ == '__main__':
     gold_annotation_data = get_annotation_data(gold_annotation_file, gold=True)
     pred_annotation_data = get_annotation_data(pred_annotation_file, gold=False)
 
-    TP, FP, FN = get_stat(gold_annotation_data, pred_annotation_data)
+    TP, FP, FN, TP_sents, FP_sents, FN_sents = get_stat(gold_annotation_data, pred_annotation_data)
 
-    print "TP: " + str(TP)
-    print "FP: " + str(FP)
-    print "FN: " + str(FN)
+    print_stats(TP, FP, FN)
+    write_stat2file(TP_sents, FP_sents, FN_sents)
 
-    precision = TP/(TP+FP)
-    recall = TP/(TP+FN)
-    f1 = 2*(recall * precision) / (recall + precision)
 
-    print
-    print "Precision = " + str(precision)
-    print "Recall = " + str(recall)
-    print "F1 = " + str(f1)
