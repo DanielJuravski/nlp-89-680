f5bc40645fef792cd942058693625412e8b7a3a1
eyalorbach <eyal@mac.com>
2018-12-22 15:30:11 +0200

    add filter by verb dep route

--- ex4/extract.py
+++ ex4/extract.py
@@ -1,13 +1,94 @@
 import sys
-
 import utils
 
 
-def extract_by_NER(data):
-    for sen_id in data:
+def extract_by_NER(sen):
+    relations = []
+    persons = []
+    geos = []
+
+    for ent in sen.doc.ents:
+        if ent.label_ == 'PERSON':
+            persons.append((ent.text, ent))
+        elif ent.label_ == 'GPE':
+            geos.append((ent.text, ent))
+
+    persons = filter_persons_by_compnoun(persons, sen)
+
+    for person in persons:
+        for geo in geos:
+            relations.append((person, geo))
+
+
+    return relations
+
+
+def filter_persons_by_compnoun(persons, sen):
+    for token in sen.doc:
+        # print(token.text, token.dep_, token.head.text, token.head.pos_, [child for child in token.children])
+        if token.dep_ == 'compound' and token.head.pos_ == 'NOUN':
+            for p in persons:
+                if p[0] == token.text:
+                    persons.remove(p)
+    return persons
+
+
+def check_pos_in_children(head, uphill_child, geo_entity):
+    if head == geo_entity.root:
+        return True
+
+    found = False
+    for child in head.children:
+        if child != uphill_child:
+            found = found or check_pos_in_children(child, uphill_child, geo_entity)
+
+    return found
+
+
+def filter_by_dep_tree_via_verb(sen_relations, sen):
+    filtered = []
+    found = False
+    for relation in sen_relations:
+        per_entity = relation[0][1]
+        geo_entity = relation[1][1]
+        head = per_entity.root.head
+        uphill_child = per_entity.root
+        print(head.text)
+        while uphill_child != head:
+            print ("###head###")
+            print(head.text)
+            if head.pos_ == 'VERB':
+                found = found or check_pos_in_children(head, uphill_child, geo_entity)
+            print ("###children###")
+            for child in head.children:
+                print child.text
+
+            uphill_child = head
+            head = head.head
+
+
+        if found:
+            filtered.append(relation)
+
+    return filtered
+
+
+def extract_from_sentences(data):
+    relations = {}
+    sorted_sen_ids = utils.get_sorted_ids(data.keys())
+    for sen_id in sorted_sen_ids:
         sen = data[sen_id]
-        print sen_id
-        print sen.text
+        sen_relations = extract_by_NER(sen)
+        sen_relations = filter_by_dep_tree_via_verb(sen_relations, sen)
+        if len(sen_relations) > 0:
+            print sen_id
+            for token in sen.doc:
+                print(token.text, token.dep_, token.head.text, token.head.pos_, [child for child in token.children])
+        relations[sen_id] = [(r[0][0], r[1][0]) for r in sen_relations]
+
+    return relations
+
+
 
 
 if __name__ == '__main__':
@@ -15,5 +96,6 @@ if __name__ == '__main__':
     output_file = sys.argv[2]
     data = {}
     for sen_id, sen in utils.read_lines(sys.argv[1]):
-        data[sen_id] = sen
-    relations = extract_by_NER(data)
+        data[sen_id] = utils.nlp(sen)
+    relations = extract_from_sentences(data)
+    utils.print_relations(relations, data, output_file)
--- ex4/utils.py
+++ ex4/utils.py
@@ -2,7 +2,7 @@ import codecs
 import spacy
 import sys
 
-
+SENT_PREFIX="sent"
 nlp = spacy.load('en')
 
 def read_lines(fname):
@@ -11,3 +11,21 @@ def read_lines(fname):
         sent = sent.replace("-LRB-","(")
         sent = sent.replace("-RRB-",")")
         yield sent_id, sent
+
+
+def print_relations(relations, full_data, out_f):
+    sorted_sen_ids = sorted([int(key[len(SENT_PREFIX):]) for key in relations.keys()])
+    with open(out_f, "w") as f:
+        for sen_id in sorted_sen_ids:
+            senid_str = SENT_PREFIX + str(sen_id)
+            sen_relations = relations[senid_str]
+            for relation in sen_relations:
+                relation_txt = "%s\tLive_In\t%s" % (relation[0], relation[1])
+                full_sen = full_data[senid_str].text
+                output_line = "%s\t%s\t(%s)\n" % (senid_str, relation_txt, full_sen)
+                f.write(output_line)
+
+
+def get_sorted_ids(sen_str_ids):
+    sorted_ids =  sorted([int(key[len(SENT_PREFIX):]) for key in sen_str_ids])
+    return [SENT_PREFIX + str(sen_id) for sen_id in sorted_ids]
